e40781b0cb48b0b015b5c99fead2f80e21d4f3ab
riban <riban@zynthian.org>
2024-12-26 10:53:34 +0000

    Force point release tags

--- scripts/point_release.sh
+++ scripts/point_release.sh
@@ -13,11 +13,7 @@ for REPO in zynthian-sys zynthian-ui zynthian-webconf zynthian-data zyncoder
 do
   git -C /zynthian/$REPO checkout oram
   git -C /zynthian/$REPO pull
-  git -C /zynthian/$REPO tag $TAG
-  if [ $? != 0 ]
-  then
-    echo TAG already assigned to $REPO
-  fi
+  git -C /zynthian/$REPO tag -f $TAG
   git -C /zynthian/$REPO tag -f oram-2409
   git -C /zynthian/$REPO push --tags --force
 done
