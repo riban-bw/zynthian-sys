f97bff70dd8e37031b2a992c776b8451bfa0b43d
riban <riban@zynthian.org>
2024-12-26 08:47:05 +0000

    Adds script to tag point release

---
+++ scripts/point_release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Check if the number of arguments is not equal to 1
+if [ "$#" -ne 1 ]; then
+    echo Error: Expects name of point release as command line argument, e.g. oram-2024.1.0.0
+    exit 1
+fi
+
+TAG=$1
+
+for REPO in zynthian-sys zynthian-ui zynthian-webconf zynthian-data zyncoder
+do
+  git -C /zynthian/$REPO checkout oram
+  git -C /zynthian/$REPO pull
+  git -C /zynthian/$REPO tag $TAG
+  if [ $? != 0 ]
+  then
+    echo TAG already assigned to $REPO
+  fi
+  git -C /zynthian/$REPO tag -f oram-2409
+  git -C /zynthian/$REPO push --tags --force
+done
